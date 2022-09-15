3c23ff2e7e80d9213b483a9671b38902570fc283
Pedro Sátiro Correia <phsatiroc03@gmail.com>
2022-09-15 18:57:51 -0300

    -

--- C/Lista 02/11- Analise de Credito.c
+++ C/Lista 02/11- Analise de Credito.c
@@ -4,7 +4,18 @@
 #include <stdlib.h>
 
 int main() {
-    double salario, renda;
+    double salario, renda,limite, sobra;
+    scanf("%lf %lf", &salario, &renda);
+
+    limite = salario* 0.30;
+    sobra = limite - renda;
+
+    if (sobra>=0){
+        printf("%.2lf", sobra);
+    } else{
+        printf("0.00");
+    }   
+
 
 	return 0;
 }
--- C/Lista 02/14-Conta de Água.c
+++ C/Lista 02/14-Conta de Água.c
@@ -4,9 +4,24 @@
 #include <stdlib.h>
 
 int main() {
+	int consumo,conta=7;
+	scanf("%d",&consumo);
+	int a = consumo - 10;
 
-
-
+	if (a > 0 ){
+		int b = a - 20;
+		if(b > 0 ){
+			int c = b - 70;
+			if (c > 0){
+				conta += (c*5)+160;
+			} else {
+				conta += ((c+70)*2)+20;
+			}
+		}else {
+			conta += (b+20);
+		}
+	}
+	printf("%d", conta);
     
 	return 0;
 }
